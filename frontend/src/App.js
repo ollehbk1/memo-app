dafe126fed6466a31b04d7c59d1638e8d7339a53
ollehbk1 <ollehbk@gmail.com>
2025-06-23 19:13:07 +0900

    edit code : test for automatically build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 function App() {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold text-indigo-600">📝 나의 메모장 (React)</h1>
+      <h1 className="text-2xl font-bold text-indigo-600">📝 나만의 메모장 (React)</h1>
     </div>
   );
 }
